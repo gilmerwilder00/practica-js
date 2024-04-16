1a14ed11895352539292fff4e38ac7bbf933931b
gilmerwilder00 <gilmerwilder00@gmail.com>
2024-04-15 20:46:16 -0500

    changes

--- store/index.js
+++ store/index.js
@@ -1,18 +1,34 @@
-
+const cantidadProductos = prompt("¿Cuantos productos desea comprar?");
 
 let totalApagar = 0;
+let nombresProductos = "";
 
-for(let index=1; index <= 2; index++ ){
+for (let index = 1; index <= cantidadProductos; index++) {
+  const nombre = prompt("¿Qué producto desea llevar?");
+  nombresProductos = nombresProductos + nombre + ",";
+  const cantidad = Number(prompt("¿Cuántas unidades?"));
+  const precio = Number(prompt("¿Cuánto sale cada unidad?"));
+  const subtotal = cantidad * precio;
 
-    const nombre = prompt("¿Qué producto desea llevar?");
-    const cantidad = Number(prompt("¿Cuántas unidades?"));
-    const precio = Number(prompt("¿Cuánto sale cada unidad?"));
-    const subtotal = cantidad * precio;
+  totalApagar = totalApagar + subtotal;
 
-    totalApagar = totalApagar + subtotal;
-
-    // totalApagar += subtotal;
-
+  // totalApagar += subtotal;
 }
 
-alert('El total de la sumatoria es : ' + totalApagar);
+// nombresProductos= 'cuaderno,lapiz,tijera,'
+
+nombresProductos = nombresProductos.substring(0, nombresProductos.length - 1);
+
+const mensaje =
+  "Productos: " +
+  nombresProductos +
+  " \n El total de la sumatoria es :  " +
+  totalApagar;
+
+alert(mensaje);
+
+// let nombresProductos= 'cuaderno,lapiz,tijera,';
+
+// nombresProductos = nombresProductos.substring(0, 14 );
+
+// alert(nombresProductos);
